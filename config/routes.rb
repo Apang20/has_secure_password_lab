3898241f1eb243f06b392360cf91f25c09d0cfe5
github username <github email address>
2020-12-28 18:09:31 -0600

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/login', to: 'sessions#new', as: 'new_session'
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
+
+  resources :users, only: [:show, :new, :create]
 end
